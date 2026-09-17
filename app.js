bb762bb7b0081f1b51eb3957f15c20672da8ac8a
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2026-09-17 16:29:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
+        executablePath: process.env.CHROME_PATH || undefined,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
