7832398f89f4e3d96376cdf1031b65c8ca12d643
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2026-09-17 16:19:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ let clientStatus = 'Desconectado';
 // Inicializa WhatsApp Web
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // <- Este argumento es clave en servidores compartidos con pocos recursos
+            '--disable-gpu'
+        ],
+    }
 });
 
 client.on('qr', (qr) => {
